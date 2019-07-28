384511c5463278f958ea094dc67ce0cb413fcfb5
Rhythm Solutions <rhythmsolutions2017@gmail.com>
2019-07-28 20:13:59 +0530

    Firebase initiated

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+//const Color kactiveColor = Color(0xFF00264A);
+//const Color kinactiveColor = Color(0xFF01417d);
+const Color kbackgroundEndColor = Color(0xFF0282fa);
+const Color kbackgroundStartColor = Color(0xFF000912);
+const Color kactiveColor = Colors.white;
+const Color kinactiveColor = Colors.white70;
--- lib/screens/lubeFinder_screen.dart
+++ lib/screens/lubeFinder_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hp_lubricants/constants.dart';
 
 class LubeFinderScreen extends StatefulWidget {
   static String id = 'lubeFinder_screen';
@@ -19,6 +20,143 @@ class _LubeFinderScreenState extends State<LubeFinderScreen> {
         ),
         leading: Image.asset(
           'assets/images/icon_hplubricrant.png',
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: <Color>[
+          kbackgroundStartColor,
+          kbackgroundEndColor,
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Container(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Expanded(
+                      child: new GridButton(
+                        color: kactiveColor,
+                      ),
+                    ),
+                    Expanded(
+                      child: new GridButton(
+                        color: kinactiveColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Expanded(
+                      child: new GridButton(
+                        color: kinactiveColor,
+                      ),
+                    ),
+                    Expanded(
+                      child: new GridButton(
+                        color: kinactiveColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(
+                child: ListView(
+                  // This next line does the trick.
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.all(30.0),
+                      width: 200.0,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
+                        border: Border.all(
+                            color: Colors.blue,
+                            width: 2.0,
+                            style: BorderStyle.solid),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(30.0),
+                      width: 200.0,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
+                        border: Border.all(
+                            color: Colors.blue,
+                            width: 2.0,
+                            style: BorderStyle.solid),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(30.0),
+                      width: 200.0,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
+                        border: Border.all(
+                            color: Colors.blue,
+                            width: 2.0,
+                            style: BorderStyle.solid),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class GridButton extends StatelessWidget {
+  final Color color;
+
+  GridButton({this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        margin: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10.0),
+          border: Border.all(
+              color: Colors.black12, width: 2.0, style: BorderStyle.solid),
+        ),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Text("TYPE OF CAR"),
+            ),
+            Expanded(
+              flex: 2,
+              child: Icon(Icons.motorcycle),
+            ),
+            Expanded(
+              flex: 1,
+              child: Text("TWO WHEELER"),
+            ),
+          ],
         ),
       ),
     );
